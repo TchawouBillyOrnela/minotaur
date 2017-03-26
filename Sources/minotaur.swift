6a97882e4be43a8c2ea3ff6870c540da1e853fd7
Ornela Tchawou <Ornela.Tchawou@etu.unige.ch>
2017-03-26 01:56:23 +0100

    premiere version

--- Sources/minotaur.swift
+++ Sources/minotaur.swift
@@ -38,29 +38,58 @@ func room (_ x: Int, _ y: Int) -> Term {
 }
 
 func doors (from: Term, to: Term) -> Goal {
+  return (from.1 === 4 && from.2 === 4 ) && (to.1 === 4 && to.2 === 3)||
+(from.1 === 4 && from.2 === 3 ) && ( (to.1 === 4 && to.2 === 2)|| (to.1 === 3 && to.2 === 3))||
+(from.1 === 4 && from.2 === 2) &&  (to.1 === 3 && to.2 === 2)||
+(from.1 === 4 && from.2 === 1 ) && (to.1 === 3 && to.2 === 1)||
+(from.1 === 3 && from.2 === 2 ) && ((to.1 === 3 && to.2 === 1)||(to.1 === 2 && to.2 === 2))||
+(from.1 === 3 && from.2 === 1 ) && (to.1 === 2 && to.2 === 1)||
+(from.1 === 2 && from.2 === 1 ) && ((to.1 === 1 && to.2 === 1)||(to.1 === 2 && to.2 === 2))||
+(from.1 === 2 && from.2 === 2 ) && (to.1 === 2 && to.2 === 3)||
+(from.1 === 2 && from.2 === 3 ) && ((to.1 === 2 && to.2 === 4)||(to.1 === 3 && to.2 === 3))||
+(from.1 === 2 && from.2 === 4) && ((to.1 === 3 && to.2 === 4)||(to.1 === 1 && to.2 === 4))||
+(from.1 === 1 && from.2 === 4 ) && (to.1 === 1 && to.2 === 3)||
+(from.1 === 1 && from.2 === 3 ) && (to.1 === 1 && to.2 === 2)||
+(from.1 === 1 && from.2 === 2 ) && (to.1 === 1 && to.2 === 1)
     // TODO
 }
 
 func entrance (location: Term) -> Goal {
+  return (location.1 === 4 && location.2 === 1)||
+  (location.1 = 4 && location.2 === 4)
     // TODO
 }
 
 func exit (location: Term) -> Goal {
+  return (location.1 === 1 && location.1 === 1)||
+  (location.1 = 3 && location.1 === 4)
     // TODO
 }
 
 func minotaur (location: Term) -> Goal {
+  return (location.1 === 2 && location.2 === 3)
     // TODO
 }
 
 func path (from: Term, to: Term, through: Term) -> Goal {
+  return (from.1 === to.1 && from.2 === to.2 && through === List.empty )||
+  (doors(from, to) && through === List.empty )||
+  (doors(from, through) && doors(through, to) )||
+  delayed fresh{ch in fresh {lc in
+  (path(from, through, ch ) && path(through, to, lc))
+  }}
     // TODO
 }
 
 func battery (through: Term, level: Term) -> Goal {
+  return (through === List.empty && ((level === 0) ||delayed fresh {y in (level === succ(y) ) )) ||
+  delayed fresh{ch in fresh {lc in fresh {n in
+    (through === List.const(ch, lc) && (level === succ (n)) && winning (lc, n) )
     // TODO
 }
 
 func winning (through: Term, level: Term) -> Goal {
+  return ( battery(through, level) && ( path( (4,1), (3,4), (2,3)) || path( (4,1), (1,1), (2,3) ) ) )||
+   ( battery(through, level) && ( path( (4,4), (3,4), (2,3) ) || path( (4,4), (1,1), (2,3) ) ) )
     // TODO
 }
